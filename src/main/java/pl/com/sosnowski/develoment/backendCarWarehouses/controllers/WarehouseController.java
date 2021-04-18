25a1467297a87226b9239d866449965585a8e9c5
Pawel S <psosnows@gmail.com>
2021-04-18 16:10:40 +0200

    fixing warning output...

--- src/main/java/pl/com/sosnowski/develoment/backendCarWarehouses/controllers/WarehouseController.java
+++ src/main/java/pl/com/sosnowski/develoment/backendCarWarehouses/controllers/WarehouseController.java
@@ -131,7 +131,7 @@ public class WarehouseController {
         try {
             Optional<Warehouse> warehouse = warehouseRepository.findById(id);
             if(warehouse.isEmpty()) {
-                log.log(Level.WARNING, "Did not find warehouse with id"+id);
+                log.log(Level.WARNING, "Did not find warehouse with id: "+id);
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
             return new ResponseEntity<>(warehouse, HttpStatus.OK);
