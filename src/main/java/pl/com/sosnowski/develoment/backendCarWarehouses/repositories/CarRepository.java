aee420760240d6ee35efaad0c835db141ef8693f
Pawel S <psosnows@gmail.com>
2021-04-18 16:01:41 +0200

    fixing stupid Pageable import...

--- src/main/java/pl/com/sosnowski/develoment/backendCarWarehouses/repositories/CarRepository.java
+++ src/main/java/pl/com/sosnowski/develoment/backendCarWarehouses/repositories/CarRepository.java
@@ -1,10 +1,10 @@
 package pl.com.sosnowski.develoment.backendCarWarehouses.repositories;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import pl.com.sosnowski.develoment.backendCarWarehouses.entities.Car;
 
-import java.awt.print.Pageable;
 
 public interface CarRepository extends MongoRepository<Car, Long> {
     Page<Car> findAll(Pageable pageable);
